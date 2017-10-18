7980dd9f44a992926cc9e891d8a71c9ccd977f6c
Christie Lynam <christielynam@gmail.com>
2017-10-18 11:26:19 -0600

    Fix sizing of page Refactor javascript

--- scripts.js
+++ scripts.js
@@ -1,26 +1,8 @@
-const showActiveTab = () => {
-  $('#tab-one').hasClass('active') ?
-  $('#tab-one-content').addClass('active-tab') :
-  $('#tab-one-content').removeClass('active-tab');
-
-  $('#tab-two').hasClass('active') ?
-  $('#tab-two-content').addClass('active-tab') :
-  $('#tab-two-content').removeClass('active-tab');
-
-  $('#tab-three').hasClass('active') ?
-  $('#tab-three-content').addClass('active-tab') :
-  $('#tab-three-content').removeClass('active-tab');
-
-  $('#tab-four').hasClass('active') ?
-  $('#tab-four-content').addClass('active-tab') :
-  $('#tab-four-content').removeClass('active-tab');
-}
-
-
 $('.tab').on('click', function() {
   $('.tab').removeClass('active');
+  $('.content-container').removeClass('active-tab')
   $(this).addClass('active');
   $('.tab span').text('+');
   $(this).find('span').text('-');
-  showActiveTab();
+  $(this).next().addClass('active-tab');
 })
